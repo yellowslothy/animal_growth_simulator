b105694f99f687bbbc1dd4ffdbcc65e4f244274e
yellowslothy <yellowslothy3@gmail.com>
2025-06-06 22:55:01 +0900

    Update animals.py

--- animals.py
+++ animals.py
@@ -2,23 +2,28 @@ class Animal:
     def __init__(self, name):
         self.name = name
         self.age = 0
-        self.exp = 0
         self.level = 1
-
-    def feed(self):
-        self.exp += 10
-        if self.exp >= self.level * 50:
-            self.level_up()
-
-    def level_up(self):
-        self.level += 1
-        self.exp = 0
-        self.age += 1
+        self.experience = 0
+        self.is_alive = True
 
     def status(self):
         return {
-            "Name": self.name,
             "Age": self.age,
             "Level": self.level,
-            "Experience": self.exp
+            "Experience": self.experience,
         }
+
+    def feed(self, fish=False):
+        if not self.is_alive:
+            return
+        if fish:
+            self.experience += 10
+            if self.experience >= self.level * 50:
+                self.level_up()
+
+    def die(self):
+        self.is_alive = False
+
+    def level_up(self):
+        self.level += 1
+        self.experience = 0
